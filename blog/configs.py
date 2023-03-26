a6fbdd85860ba5e1c6f4a54f39f3b560fbe887fe
Alexey Ryzhkov <aler32@mail.ru>
2023-03-26 11:53:02 +0300

    change_config

--- blog/configs.py
+++ blog/configs.py
@@ -26,4 +26,5 @@ class TestingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
